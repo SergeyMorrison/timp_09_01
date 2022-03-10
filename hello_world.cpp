7db1c5ac8430ba3afb9afaf519234ec251f1cbce
SergeyMorrison <morrisYT@yandex.ru>
2022-03-10 23:51:39 +0300

    changes patch_2

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 
-int main ()
+int
+main()
 {
-std::string @name;
-std::cout << "Name: ";//комментарий для пользователя
-std::cin >> @name;//ввод
-std::cout << endl;
-std::cout << "Hello world from " << @name << std::endl;//вывод
-return 0;
+  std::string @name;
+  std::cout << "Name: "; //комментарий для пользователя
+  std::cin >> @name;     //ввод
+  std::cout << endl;
+  std::cout << "Hello world from " << @name << std::endl; //вывод
+  return 0;
 }
